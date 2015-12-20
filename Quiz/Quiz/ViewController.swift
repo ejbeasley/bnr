01ebdb92b5eea88db8b7c67152ecb5f42c8ce6f9
eric beasley <ejbeasley@gmail.com>
2015-12-20 13:13:16 -0800

    Working on Chapter 1

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Eric Beasley on 12/18/15.
+//  Copyright © 2015 eric j beasley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNexQuestion(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
+}
+
+
