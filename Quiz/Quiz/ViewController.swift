848ba1389d901f14216c956c51d754e035d485dd
eric beasley <ejbeasley@gmail.com>
2015-12-26 08:41:49 -0800

    Chapter 1 completed

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -12,12 +12,29 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes", "14", "Montepelier"]
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNexQuestion(sender: AnyObject) {
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
         
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
-        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
     }
 }
 
